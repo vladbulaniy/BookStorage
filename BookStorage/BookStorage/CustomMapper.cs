4e4dcd6efff38199c69bf41c5877232020cc9e1f
vladbulanyy <vladbulanyy@gmail.com>
2020-04-28 23:59:21 +0300

    Start to implemetn custom automapprer

---
+++ BookStorage/BookStorage/CustomMapper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace BookStorage
+{
+    public static class CustomMapper
+    {
+        public static D Map<S, D>(S source, D destination)
+        {
+            D result;
+            var sourceFields = GetListFields(S);
+            var destinationFields = GetListFields(D);
+
+            foreach (var sField in sourceFields.Select((value, i) => new { i, value }))
+            {
+                var sFieldName = sField.value;
+                var index = sField.i;
+
+                if (sFieldName == destinationFields[index])
+                {
+                    result[sFieldName] = destinationFields[index];
+                }
+            }
+
+            return result;
+        }
+
+        public static List<string> GetListFields(Type t)
+        {
+            var result = from f in t.GetFields() select f.Name;
+            return result.ToList();
+        }
+    }
+}
